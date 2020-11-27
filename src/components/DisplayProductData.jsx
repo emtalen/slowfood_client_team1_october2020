164fc2bb41fc842ec401071db53400600172f342
emtalen <59577366+emtalen@users.noreply.github.com>
2020-11-27 18:13:49 +0100

    reafactores, moves functions to parent component Home and updates the state of productData with the products in a specific category

--- src/components/DisplayProductData.jsx
+++ src/components/DisplayProductData.jsx
@@ -1,35 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 
-class DisplayProductData extends Component {
-	// state = {
-	// 	productData: [],
-	// };
+// refactored this to a functional component
+const DisplayProductData = (props) => {
+  let dataIndex;
+  const data = props.productData;
+  if (Array.isArray(data) && data) {
+    dataIndex = (
+      <div id="index">
+        {data.map((item) => {
+          return (
+            <div key={item.id}>
+              {item.name},{item.description},{item.price}
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return (
+      <p id="message">Sorry! Unfortunately we have issues with our menu</p>
+    );
+  }
 
-
-
-	render() {
-		let dataIndex;
-		const data = this.props.productData;
-		if (Array.isArray(data) && data) {
-			dataIndex = (
-				<div id="index">
-					{data.map((item) => {
-						return (
-							<div key={item.id}>
-								{item.name},{item.description},{item.price}
-							</div>
-						);
-					})}
-				</div>
-			);
-		} else {
-			return (
-				<p id="message">Sorry! Unfortunately we have issues with our menu</p>
-			);
-		}
-
-		return <div>{dataIndex}</div>;
-	}
-}
+  return <div>{dataIndex}</div>;
+};
 
 export default DisplayProductData;
